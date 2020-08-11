64473089c0bf98025207920b38a20a2a2d253819
Javier Hortal Amador <jhortal@tychegroup.es>
2020-08-11 16:28:26 +0200

    correct client close

--- src/bck/handlers/get-patients.js
+++ src/bck/handlers/get-patients.js
@@ -9,7 +9,7 @@ const test = require('assert');
 // Connection url
 
 const url = config.get('bck.dbConnectionURL');
-const client = new MongoClient(url);
+const client = new MongoClient(url, { useUnifiedTopology: true });
 // Database Name
 
 // Connect using MongoClient
@@ -17,70 +17,68 @@ const client = new MongoClient(url);
 
 // Connect using MongoClient
 exports.getAllPatientsHandler = async (event) => {
-    try {
-        await client.connect();
-    
-        const database = client.db("clinica");
-        const collection = database.collection("pacientes");
-    
-        console.log("Conected to MongoDB")
-    
-        const cursor = collection.find();
-    
-        // print a message if no documents were found
-        if ((await cursor.count()) === 0) {
-            console.log("No documents found!");
-        }
 
-        var body=[]
-        for await (const doc of cursor) {
-            body.push(doc);
-        }
-        var response = {
-            headers: {
-                "Content-Type": "application/json"
-            },
-            statusCode: 200,
-            body: JSON.stringify(body)
-        };
-        console.log("Response" + JSON.stringify(response))
-        return response
-    } finally {
-        await client.close();
+    await client.connect();
+
+    const database = client.db("clinica");
+    const collection = database.collection("pacientes");
+
+    console.log("Conected to MongoDB")
+
+    const cursor = collection.find();
+
+    // print a message if no documents were found
+    if ((await cursor.count()) === 0) {
+        console.log("No documents found!");
     }
-    
+
+    var body = []
+    for await (const doc of cursor) {
+        body.push(doc);
+    }
+
+    await client.close()
+
+    var response = {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        statusCode: 200,
+        body: JSON.stringify(body)
+    };
+    console.log("Response" + JSON.stringify(response))
+    return response
+
+
 }
 
 exports.getPatientByIdHandler = async (event) => {
-    try {
-        var PatientData = new PatientModel(event.body);
+    var PatientData = new PatientModel(event.body);
 
-        await client.connect();
-    
-        const database = client.db("clinica");
-        const collection = database.collection("pacientes");
-    
-        console.log("Conected to MongoDB")
+    await client.connect();
 
-        var query = {
-            _id: mongodb.ObjectId(PatientData.idDocument)
-        }
-    
-        var cursor = await collection.findOne(query);
-        
-        var body = cursor ? cursor : {data:"empty"}
-        
-        var response = {
-            headers: {
-                "Content-Type": "application/json"
-            },
-            statusCode: 200,
-            body: JSON.stringify(body)
-        };
-        console.log("Response" + JSON.stringify(response))
-        return response
-    } finally {
-        await client.close();
+    const database = client.db("clinica");
+    const collection = database.collection("pacientes");
+
+    console.log("Conected to MongoDB")
+
+    var query = {
+        _id: mongodb.ObjectId(PatientData.idDocument)
     }
-    
+
+    var cursor = await collection.findOne(query).then(client.close());
+
+    var body = cursor ? cursor : { data: "empty" }
+
+    var response = {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        statusCode: 200,
+        body: JSON.stringify(body)
+    };
+    console.log("Response" + JSON.stringify(response))
+    return response
+
+
 }
